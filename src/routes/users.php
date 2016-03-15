f14c5e1a84e53424bbebb293491cddd6fa777a57
Peter Clotworthy <clotters@gmail.com>
2016-03-15 15:46:30 +0000

    Users route additions

--- src/routes/users.php
+++ src/routes/users.php
@@ -1,5 +1,6 @@
 <?php
 
+use NorthEastEvents\User;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
@@ -21,7 +22,26 @@ $app->group('/api', function() use ($app) {
         return $response;
     });
 
-    $app->map(["GET", "DELETE", "PUT", "PATCH"], '/user/{id:[0-9]+}', function (Request $request, Response $response, $args) use ($app) {
+    $app->post('/user', function (Request $request, Response $response) {
+        $user = new User();
+        $userjson = $request->getParsedBody()['user'];
+        $userjson = json_decode($userjson, true);
+        if(array_key_exists('FirstName', $userjson))
+            $user->setFirstName($userjson['FirstName']);
+        if(array_key_exists('LastName', $userjson))
+            $user->setLastName($userjson['LastName']);
+        if(array_key_exists('AvatarUrl', $userjson))
+            $user->setAvatarUrl($userjson['AvatarUrl']);
+        if(array_key_exists('Permission', $userjson)) {
+            if(stripos('normal', $userjson['Permission']) !== FALSE)
+                $user->setAvatarUrl(\NorthEastEvents\Map\UserTableMap::COL_PERMISSION_NORMAL);
+            if(stripos('staff', $userjson['Permission']) !== FALSE)
+                $user->setAvatarUrl(\NorthEastEvents\Map\UserTableMap::COL_PERMISSION_NORMAL);
+        }
+        $user->save();
+    });
+
+    $app->map(["GET", "DELETE", "PUT", "PATCH", "POST"], '/user/{id:[0-9]+}', function (Request $request, Response $response, $args) use ($app) {
         $user = \NorthEastEvents\Base\UserQuery::create()->findOneById($args['id']);
         if($request->isGet())
             $response->getBody()->write($user->toJSON());
@@ -30,9 +50,20 @@ $app->group('/api', function() use ($app) {
             $response->getBody()->write("User deleted? ".  ($user->isDeleted()? "Yes" : "No"));
         }
         if($request->isPost() || $request->isPut() || $request->isPatch()){
-            $userjson = $_POST["user"];
+            $userjson = $request->getParsedBody()['user'];
             $userjson = json_decode($userjson, true);
-             
+            if(array_key_exists('FirstName', $userjson))
+                $user->setFirstName($userjson['FirstName']);
+            if(array_key_exists('LastName', $userjson))
+                $user->setLastName($userjson['LastName']);
+            if(array_key_exists('AvatarUrl', $userjson))
+                $user->setAvatarUrl($userjson['AvatarUrl']);
+            if(array_key_exists('Permission', $userjson)) {
+                if(stripos('normal', $userjson['Permission']) !== FALSE)
+                    $user->setAvatarUrl(\NorthEastEvents\Map\UserTableMap::COL_PERMISSION_NORMAL);
+                if(stripos('staff', $userjson['Permission']) !== FALSE)
+                    $user->setAvatarUrl(\NorthEastEvents\Map\UserTableMap::COL_PERMISSION_STAFF);
+            }
         }
 
         return $response;
--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,16 +1,21 @@
 <?php
 
-//require '../vendor/autoload.php';
+require '../src/vendor/autoload.php';
 use \NorthEastEvents\User;
 
 class UserTest extends PHPUnit_Framework_TestCase {
-    public function testStaffPermissions()
-    {
+    public function testStaffPermissions(){
         $user = new User();
         $user->setPermission(\NorthEastEvents\Map\UserTableMap::COL_PERMISSION_STAFF);
         $this->assertTrue(User::isAdmin($user));
 
         $user->setPermission(\NorthEastEvents\Map\UserTableMap::COL_PERMISSION_NORMAL);
         $this->assertFalse(User::isAdmin($user));
+    }
+
+    public function userAPIRead(){
+        $user = new User();
+        $faker = Faker\Factory::create();
+        // TODO: finish this test
     }
 }
