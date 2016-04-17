d823231129befd9b67f35b010a82c7e819c44234
Peter Clotworthy <clotters@gmail.com>
2016-04-18 00:58:30 +0100

    Charity routes

--- src/NorthEastEvents/Controllers/CharityController.php
+++ src/NorthEastEvents/Controllers/CharityController.php
@@ -8,7 +8,36 @@
 
 namespace NorthEastEvents\Controllers;
 
+use NorthEastEvents\Models\CharityQuery;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
 
 class CharityController extends Controller {
+    public function CharitiesList(Request $req, Response $res, $args){
+        return $this->render($req, $res, "/charities/charities.html.twig", [
+           'charities' => CharityQuery::create()->find()
+        ]);
+    }
+    
+    public function CreateCharityGET(Request $req, Response $res, $args){
+        return $this->render($req, $res, "/charities/create.html.twig", [
 
+        ]);
+    }
+
+    public function CreateCharityPOST(Request $req, Response $res, $args){
+
+    }
+    
+    public function CharityOperations(Request $req, Response $res, $args){
+        $charity = CharityQuery::create()->findOneById($args["charityID"] ?? null);
+        if($charity == null){
+            return $this->NotFound("Could not find a charity with the provided information.", $req, $res, $args);
+        }
+
+        return $this->render($req, $res, "/charities/charity.html.twig", [
+            'charity' => $charity
+        ]);
+    }
 }
--- src/NorthEastEvents/Controllers/Routes/CharityRoutes.php
+++ src/NorthEastEvents/Controllers/Routes/CharityRoutes.php
@@ -9,10 +9,31 @@
 namespace NorthEastEvents\Controllers\Routes;
 
 
+use NorthEastEvents\Controllers\CharityController;
+use NorthEastEvents\Middleware\AuthorisedRouteMiddleware;
+
 class CharityRoutes extends Routes
 {
 
     public function routes() {
         $app = $this->app;
+
+        $app->get('/charities[/{page:[0-9]+}]', CharityController::class.":CharitiesList");
+
+        $app->group('/user', function(){
+            // Create new Charity
+            $this->get('/create', CharityController::class.':CreateCharityGET')
+                ->setName("CharityCreateGET");
+
+            $this->post('/create', CharityController::class.':CreateCharityPOST')
+                ->setName("CharityCreatePOST")->add(new AuthorisedRouteMiddleware($this->getContainer()));
+
+            // Operations on a specific user
+            $this->group('/{charityID:[0-9]+}', function(){
+                // Get user details
+                $this->get('', CharityController::class.':CharityOperations')
+                    ->setName("CharityOperations");
+            });
+        });
     }
 }
